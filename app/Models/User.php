bc1b924a064ecab50f3282e10be7120901297fad
RafiArizu <rafiariz95@gmail.com>
2026-09-11 01:05:47 +0700

    revisi model

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,7 +50,7 @@ class User extends Authenticatable
         ];
     }
 
-    public function vendorProfile()
+    public function vendor()
     {
         return $this->hasOne(Vendor::class);
     }
