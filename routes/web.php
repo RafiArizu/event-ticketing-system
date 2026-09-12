1ee81876ddb348578d0c8e6bc8f92b621799b33f
RafiArizu <rafiariz95@gmail.com>
2026-09-12 15:12:31 +0700

    revisi halaman categories

--- routes/web.php
+++ routes/web.php
@@ -50,8 +50,17 @@ Route::get('/admin/events/{event}', [EventController::class, 'show'])
     ->middleware(['auth', 'role:admin'])
     ->name('admin.events.show');
 
+Route::view('/admin/categories', 'admin.categories.index')
+    ->name('admin.categories.index');
+
 Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
-    Route::resource('categories', CategoryController::class);
+    Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+    Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+    Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+    Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
 });
 
 Route::get('/admin/vendors', [VendorController::class, 'index'])
